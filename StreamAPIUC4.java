54e3571636b5f5555ae4fc45b0a51609df3b38c3
akashyadavazm <akashyadavazm@gmail.com>
2022-12-28 23:47:08 +0530

    UC4

---
+++ StreamAPIUC4.java
@@ -0,0 +1,15 @@
+package javaStreamAPI;
+
+import java.util.*;
+
+public class StreamAPIUC4 {
+
+	public static void main(String[] args) {
+		List<String> listS = Arrays.asList("13", "10", "17", "23", "92", "15", "53", "46", "87", "68");
+
+		listS.stream().mapToInt(num -> Integer.parseInt(num)).filter(num -> (num % 2) == 0)
+				.forEach(System.out::println);
+
+	}
+
+}
